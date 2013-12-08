a1fbf5cd59be9c3be7f9a7739fc1a10c54da0a47
adamfowleruk <adamfowleruk@gmail.com>
2013-12-08 10:46:37 +0000

    Added Node.js RosLib overrides

---
+++ src/RosLibNode-overrides.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+/*
+ * @author Adam Fowler - adam.fowler@gmail.com
+ */
+/*
+ * The following code comes from RosLibNode-overrides.js and is cat'ed in to roslibjs.js. This provides overrides for browser-specific code in RosLibJs
+ * START RosLibNode-override.js
+ */
+ 
+ 
+
+/*
+ * END RosLibNode-override.js
+ */
+ 
